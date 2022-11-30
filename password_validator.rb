70bbdfdfc0fd7a9d0ca54ecfa90ec14682c04801
Daniel Holden <daniel.holden18@gmail.com>
2022-11-30 14:44:24 +0000

    Chapter 1 Ruby Challenge

---
+++ password_validator.rb
@@ -0,0 +1,19 @@
+# Valid passowords are
+# - 8 or more characters
+# - include at least one of these characters: !, @, $, %, &
+
+def valid?(password)
+  if password.length >= 8 && password.include?('!')
+    return true
+  elsif password.length >= 8 && password.include?('@')
+    return true
+  elsif password.length >= 8 && password.include?('$')
+    return true
+  elsif password.length >= 8 && password.include?('%')
+    return true
+  elsif password.length >= 8 && password.include?('&')
+    return true
+  else
+    return false
+end
+end
